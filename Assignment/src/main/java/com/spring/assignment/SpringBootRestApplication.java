f4fbed3b8db7241ca8b72f4f8211f66c78be4ca9
vivekmah <72371475+vivekmah@users.noreply.github.com>
2020-10-07 14:51:15 +0530

    Add files via upload

---
+++ Assignment/src/main/java/com/spring/assignment/SpringBootRestApplication.java
@@ -0,0 +1,17 @@
+package com.spring.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class SpringBootRestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestApplication.class, args);
+    }
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+}
